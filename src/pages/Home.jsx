e4ff417d39558fc1d995e3f490d3bf4097db909c
farrel raza <lendrafarrel@gmail.com>
2025-01-16 11:46:25 +0700

    Tambahan sedikit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { RiArrowDownDoubleFill } from "react-icons/ri";
 
 
 const Home = () => {
@@ -10,7 +11,11 @@ const Home = () => {
       </div>
       <div>
         <div id="section1" className="bg-home bg-no-repeat bg-center h-screen flex flex-col justify-center items-center">
-          <h1>Section 1</h1>
+          <div className="flex flex-col gap-y-10 text-center mt-20">
+            <h1 className="text-5xl font-medium text-center">Build Your Ultimate Laptop <br /> Setup with Ease.</h1>
+            <h2 className="text-xl font-light text-center">We Help You Choose Your Perfect Laptop Setup</h2>
+            <RiArrowDownDoubleFill className="mx-auto" size={65}/>
+          </div>
         </div>
         <div id="section2" className="h-screen flex flex-col justify-center items-center">
           <h1>Section 2</h1>
