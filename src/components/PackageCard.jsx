9cd8403ea041515b2e2e0a9f3d11a71b8d443cfb
aaltansa <arsalnaufal2007@gmail.com>
2025-01-30 09:17:07 +0700

    slicing package pages

--- src/components/PackageCard.jsx
+++ src/components/PackageCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { IoHeadsetOutline } from "react-icons/io5";
 import { FaRegKeyboard } from "react-icons/fa";
 import { BiMouseAlt } from "react-icons/bi";
-import { CiLaptop } from "react-icons/ci";
+import { truncateText } from "../utilities/TruncateText";
+import { FiMonitor } from "react-icons/fi";
+import { FiMousePointer } from "react-icons/fi";
 
 const PackageCard = ({ tier, description, priceRange, components }) => {
   return (
@@ -19,39 +20,59 @@ const PackageCard = ({ tier, description, priceRange, components }) => {
       <div className="space-y-3 mb-20">
         {" "}
         {/* Added mb-20 for space at bottom */}
-        {components?.laptop && (
+        <div className="flex items-center text-gray-300 gap-x-2">
+          <FiMonitor className="text-accent text-xl" />
+          <div className="flex flex-col">
+            <span className="md:text-lg">Keyboard</span>
+
+            {components.monitor.monitor_1 ? (
+              <p className="text-xs font-normal text-gray-400">
+                {truncateText(components.monitor.monitor_1.model_name)}
+              </p>
+            ) : (
+              <p className="text-xs font-normal text-gray-400">
+                No Monitor Available
+              </p>
+            )}
+          </div>
+        </div>
+        {components.mouse ? (
           <div className="flex items-center text-gray-300 gap-x-2">
-            <CiLaptop className="text-accent text-2xl" />
-            <div>
-              <h1 className="text-white">Laptop</h1>
-              <span className="text-xs font-normal text-gray-400">{components.laptop.model_name}</span>
+            <FiMousePointer className="text-accent text-2xl" />
+            <div className="flex flex-col">
+              <span className="md:text-lg">Mouse</span>
+              <p className="text-xs font-normal text-gray-400">
+                {truncateText(components.mouse.model_name)}
+              </p>
             </div>
           </div>
+        ) : (
+          <p className="text-xs font-normal text-gray-400">
+            No Mouse Available
+          </p>
         )}
-        {components?.laptop && (
-          <div className="flex items-center text-gray-300 gap-x-2">
-            <IoHeadsetOutline className="text-accent text-xl" />
-            <span>Headset</span>
+        <div className="flex items-center text-gray-300 gap-x-2">
+          <FaRegKeyboard className="text-accent text-xl" />
+          <div className="flex flex-col">
+            <span className="md:text-lg">Keyboard</span>
+
+            {components.keyboard ? (
+              <p className="text-xs font-normal text-gray-400">
+                {truncateText(components.keyboard.model_name)}
+              </p>
+            ) : (
+              <p className="text-xs font-normal text-gray-400">
+                No Keyboard Available
+              </p>
+            )}
           </div>
-        )}
-        {components?.laptop && (
-          <div className="flex items-center text-gray-300 gap-x-2">
-            <FaRegKeyboard className="text-accent text-xl" />
-            <span>Keyboard</span>
-          </div>
-        )}
-        {components?.laptop && (
-          <div className="flex items-center text-gray-300 gap-x-2">
-            <BiMouseAlt className="text-accent text-xl" />
-            <span>Mouse</span>
-          </div>
-        )}
+        </div>
       </div>
 
       {/* Bottom Section */}
       <div className="absolute bottom-4 left-4 right-4 lg:bottom-6 lg:left-6 lg:right-6 flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <span className="bg-accent text-black text-xs rounded-md font-medium px-2 py-2 whitespace-nowrap">
+          <span className="bg-accent w-full text-black text-xs rounded-md font-medium px-2 py-2 whitespace-nowrap">
             {priceRange}
           </span>
           <button className="text-white text-sm font-normal hover:underline">
---
+++ src/utilities/TruncateText.js
@@ -0,0 +1,5 @@
+export const truncateText = (text, maxLength = 25) => {
+    if (!text) return "";
+    return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+  };
+  
